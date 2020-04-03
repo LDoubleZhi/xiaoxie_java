e023944f5b5cc0456901416bbf43f72303e8e9c6
LDoubleZhi <email@example.com>
2020-04-03 11:54:44 +0800

    add chapter02

---
+++ basics/src/chapter2/Chapter02.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+/**
+ * @author : liulinzhi
+ * @date: 2020/04/03/11:40
+ * @description:
+ */
+public class Chapter02 {
+    public static void main(String[] args){
+
+    }
+}
